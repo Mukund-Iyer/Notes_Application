ee8e68b4800bd3de32555bcf5a0507bee4a8fd7a
Mukund-Iyer <mukundiyer1995@gmail.com>
2025-05-21 13:48:29 +0530

    Added exit application feature. Have to add tiling mechanism, saving the data in each tile, etc.

---
+++ Notes_v1.py
@@ -0,0 +1,23 @@
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import sys
+from PyQt5.QtWidgets import QFileDialog
+
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import sys
+from PyQt5.QtWidgets import QFileDialog
+
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        uic.loadUi("Main_Window.ui", self)  # Load the .ui file
+        self.setWindowTitle("Notes v1")
+        self.actionExit.triggered.connect(self.exit_application)
+
+    def exit_application(self):
+        # Define what happens when ExitMenuItem is triggered
+        sys.exit()
+
+app = QtWidgets.QApplication(sys.argv)
+window = MyWindow()
+window.show()
+sys.exit(app.exec_())
